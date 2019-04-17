47c43a3fb7828ccb503fb7e4e8c186ec2ec2f69d
Abhishek K <abhishekakade@gmail.com>
2019-04-18 04:57:36 +0530

    Updates

---
+++ precache-manifest.c5c07aed217ab91ee5f52983387307e8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "5db40345cfdc64de1bef76d1c23bb0a2",
+    "url": "/drumkit-font-no/static/media/x.5db40345.wav"
+  },
+  {
+    "revision": "5ce6e30506b0364f5b2e",
+    "url": "/drumkit-font-no/static/css/main.15b1c310.chunk.css"
+  },
+  {
+    "revision": "18c10dd2f24e5286dae5",
+    "url": "/drumkit-font-no/static/js/runtime~main.f8130d25.js"
+  },
+  {
+    "revision": "2104ca026fb843c1b972",
+    "url": "/drumkit-font-no/static/js/2.b527b123.chunk.js"
+  },
+  {
+    "revision": "f10ba3d1c090e49d35ca5294331b68db",
+    "url": "/drumkit-font-no/static/media/q.f10ba3d1.wav"
+  },
+  {
+    "revision": "7a90c58ff284580ec7b1336e1d700adf",
+    "url": "/drumkit-font-no/static/media/w.7a90c58f.wav"
+  },
+  {
+    "revision": "b29e205797c61288f00c4ac4c30d92d8",
+    "url": "/drumkit-font-no/static/media/e.b29e2057.wav"
+  },
+  {
+    "revision": "3f9fdf5d9386ed822c55dc86c294c6a8",
+    "url": "/drumkit-font-no/static/media/a.3f9fdf5d.wav"
+  },
+  {
+    "revision": "a55ed0c560fb8e9be2b956064a149553",
+    "url": "/drumkit-font-no/static/media/s.a55ed0c5.wav"
+  },
+  {
+    "revision": "5482e8be57bfa9085210f6fe3055790f",
+    "url": "/drumkit-font-no/static/media/d.5482e8be.wav"
+  },
+  {
+    "revision": "9eb8d6f7f163403ceb2160366876e76c",
+    "url": "/drumkit-font-no/static/media/z.9eb8d6f7.wav"
+  },
+  {
+    "revision": "5ce6e30506b0364f5b2e",
+    "url": "/drumkit-font-no/static/js/main.abd4c0e2.chunk.js"
+  },
+  {
+    "revision": "bb267ae50c88ee6dc0d05208b1d60957",
+    "url": "/drumkit-font-no/static/media/c.bb267ae5.wav"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/drumkit-font-no/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/drumkit-font-no/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/drumkit-font-no/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/drumkit-font-no/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/drumkit-font-no/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/drumkit-font-no/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/drumkit-font-no/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/drumkit-font-no/static/media/tink.2a6bb6f5.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/drumkit-font-no/static/media/tom.572ed39d.wav"
+  },
+  {
+    "revision": "68d0c888d33775a5b1d969f2cdd4cb8b",
+    "url": "/drumkit-font-no/index.html"
+  }
+];
